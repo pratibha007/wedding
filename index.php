87cb942c5b5758f743eb67ea157ac19a3af821da
pratibha007 <prashrayahada@gmail.com>
2017-08-31 07:11:07 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <title></title>
+        <style>
+        body{
+            
+        background-image:url(connect.jpg);
+        background-repeat: no-repeat;
+        background-size: 1500px 590px;
+        }
+        form{
+            background-color: red;
+        }
+        </style>
+        <div class="container-header">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="wedding.jpg" alt="" height="100px" width="300px"/>  
+
+                </div>
+                <div class="col-md-8">
+                    <h1>WEDDING DREAMS</h1>
+
+                </div>
+                
+        </div>
+        </div>
+    </head>
+    
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                </div>
+            
+            <div class="row">
+                <div class="col-md-4">
+                    <form name="myForm" action="/action_page_post.php"
+    accept-charset=""onsubmit="return validateForm()" method="post">
+                        <h1><center>Register Free<br>
+                            Meet over 10 lakh Profiles</center></h1>
+                        
+                        email <input type="email" name="email"><br>
+                        password <input type="password" name="pass"><br>
+create profile for <select name="select">
+    <option value="self">Self</option>
+    <option value="son">Son</option>
+    <option value="daughter">Daughter</option>
+    <option value="relatives">Relative</option>
+</select><br>
+date of birth day<select name="select1">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="1">3</option>
+    <option value="1">4</option>
+    <option value="1">5</option>
+    <option value="1">6</option>
+    <option value="1">7</option>
+    <option value="1">8</option>
+    <option value="1">9</option>
+    <option value="1">10</option>
+    <option value="1">11</option>
+    <option value="1">12</option>
+    <option value="1">13</option>
+    <option value="1">14</option>
+    <option value="1">15</option>
+    <option value="1">16</option>
+    <option value="1">17</option><option value="1">18</option><option value="1">19</option><option value="1">20</option><option value="1">21</option><option value="1">22</option><option value="1">23</option><option value="1">24</option><option value="1">25</option><option value="1">26</option>
+<option value="1">27</option><option value="1">28</option><option value="1">29</option><option value="1">30</option>
+<option value="1">31</option><option value="1">32</option>
+</select>
+month<select name="select2">
+    <option value="1">January</option>
+    <option value="1">February</option>
+    <option value="1">March</option>
+    <option value="1">April</option>
+    <option value="1">May</option>
+    <option value="1">June</option>
+    <option value="1">July</option>
+    <option value="1">August</option><option value="1">September</option><option value="1">October</option>
+    <option value="1">November</option><option value="1">December</option>
+</select>
+year<select name="select3">
+    <option value="1">1990</option><option value="1">1991</option><option value="1">1992</option>
+<option value="1">1993</option><option value="1">1994</option><option value="1">1995</option><option value="1">1996</option>
+ 
+<option value="1">1997</option><option value="1">1998</option><option value="1">1999</option>
+<option value="1">2000</option><option value="1">2001</option><option value="1">2002</option><option value="1">2003</option>
+<option value="1">2004</option><option value="1">2005</option><option value="1">2006</option><option value="1">2007</option>
+<option value="1">2008</option>
+  
+</select>
+<br>
+height<select name="select4">
+    <option value="1">5.1</option>
+    <option value="1">5.2</option>
+    <option value="1">5.3</option>
+    <option value="1">5.4</option>
+    <option value="1">5.5</option>
+    <option value="1">5.6</option>
+    <option value="1">5.7</option>
+</select><br>
+maritial status <select name="select4">
+    <option value="1">unmarried</option>
+    <option value="1">married</option>
+    <option value="1">divorced</option>
+    <option value="1">widowed</option>
+</select><br>
+mother tongue<select name="select5">
+    <option value="1">English</option>
+    <option value="1">Hindi</option>
+    <option value="1">Nepali</option>
+    <option value="1">others</option>
+</select>
+<br>
+religion<select name="select6">
+    <option value="1">Hindu</option>
+    <option value="1">Buddhist</option>
+    <option value="1">Christian</option>
+    <option value="1">Islam</option>
+    <option value="1">Seikh</option>
+    <option value="1">Others</option>
+</select><br>
+city <input type="text" name="address"><br>
+mobile number <input type="number" name="num"><br>
+<input type="submit" name="submit">
+
+
+
+</form>
+                </div>
+            </div>
+            
+        </div>
+            <br><br><br><br><br><br>
+          <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="1.PNG" alt=""/>
+                    <p><h3>Lakhs of Genuine Members</h3><br>
+
+                        Search by location, community, profession & more. Get matches on email as per your preferences.</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="2.PNG" alt=""/>
+                    <p><h3>Verification by Visit</h3><br>
+
+Documents on Age, Address, Income etc. collected, Verified stamp added to profile</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="3.PNG" alt=""/>
+                    <p><h3>100% Privacy</h3><br>
+
+Control who can see your profile and pictures with advanced privacy settings</p>
+                </div>
+            </div>
+          </div>
+                
+            
+        
+        
+            <script src="js/jquery-3.2.0.min.js" type="text/javascript"></script>
+            <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+
